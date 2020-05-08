6d5a50d9a8aca41dac3f72eafb19100f0937f073
aferraro <aferraro308@gmail.com>
2020-05-08 11:50:38 -0300

    Commit inicial con STS.Close #1

---
+++ springBoot_aplicacion/src/main/java/com/adrianaf/springBoot_aplicacion/SpringBootAplicacionApplication.java
@@ -0,0 +1,13 @@
+package com.adrianaf.springBoot_aplicacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAplicacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAplicacionApplication.class, args);
+	}
+
+}
---
+++ springBoot_aplicacion/src/test/java/com/adrianaf/springBoot_aplicacion/SpringBootAplicacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrianaf.springBoot_aplicacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAplicacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
